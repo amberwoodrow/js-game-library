1b91b871b5bd859b19ff65c78ac44c768bf29ff9
amberwoodrow <amberwoodrow@gmail.com>
2015-08-11 22:14:52 -0600

    made working forms, next version will fix form duplication bugs

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).on('ready', function() {
   console.log('sanity check!');
+  var libraries = {};
 
   var Game = function(title, genre) {
     this.title = title;
@@ -11,13 +12,76 @@ $(document).on('ready', function() {
     this.games = [];
   };
 
-  Game.prototype.render = function(game) {
-    $('body').append(JSON.stringify(game));
+  // Game.prototype.render = function(game) {
+  //   $('').append('<li>' + JSON.stringify(game)+ '</li>');
+  // };
+
+  GameLibrary.prototype.titleToID = function() {
+    var id = this.title.toLowerCase().replace(/ /g, "");
+    return id;
   };
 
-  GameLibrary.prototype.render = function(gameLibrary) {
-    $('body').append(JSON.stringify(gameLibrary));
+  GameLibrary.prototype.renderLibrary = function() {
+    var id = this.titleToID();
+    console.log(id);
+
+    $('#title').append("<h4>" + this.title + "</h4><table class='table table-striped' id='"+ id + "-table' >" +
+        "<thead>" +
+          "<tr>" +
+            "<th>Title</th>" +
+            "<th>Genre</th>" +
+            "<th>Delete</th>" +
+          "</tr>" +
+        "</thead>" +
+        "<tbody id=" + id + ">" +
+        "</tbody>" +
+      "</table>" +
+      '<button type="button" class="btn btn-success" id="'+ id +'-btn">' +
+        '<span class="glyphicon glyphicon-plus">' + 
+        '</span> ' +
+        'Add game' + 
+      '</button>'
+      );
+
+    $('#'+id+'-btn').click(function() {
+      $('<div>' +
+          '<form class="form-inline" id="' + id + 'addGame">' +
+            '<div class="form-group">' +
+              '<input type="title" class="form-control newGameTitle" placeholder="Game title">' +
+            '</div>' +
+            '<div class="form-group">' +
+              '<input type="genre" class="form-control newGameGenre" placeholder="Genre">' +
+            '</div>' +
+            '<button type="submit" class="btn btn-default addGameSubmit">Add game</button>' +
+          '</form>' +
+        '</div>').insertBefore(('#'+id+'-btn')); //slide btn down to display add game form.
+
+      $('#' + id + 'addGame').submit(function(e){
+        e.preventDefault();
+        var gameTitle = $(this).find('.newGameTitle').val();
+        var gameGenre = $(this).find('.newGameGenre').val();
+        var newGame = new Game(gameTitle, gameGenre);
+        libraries[id].addGame(newGame);
+        libraries[id].renderGame();
+        console.log(libraries);
+      });
+    });
   };
+
+  GameLibrary.prototype.renderGame = function() {
+    var id = "#" + this.titleToID();
+    $(id).html("");
+    for (var i=0;i<this.games.length; i++) {
+      $(id).append(
+        "<tr>" +
+          "<td>" + this.games[i].title + "</td>" +
+          "<td>" + this.games[i].genre + "</td>" +
+          "<td> <span class='remove' id=" + this.games[i].title + ">Delete</span></td>" +
+        "</tr>");
+      // if I find it, to remove it with unique id
+    }
+  };
+
 
   GameLibrary.prototype.addGame = function(game) {
     this.games.push(game);
@@ -31,14 +95,39 @@ $(document).on('ready', function() {
     }
   };
 
-  var game = new Game('Game Title', 'game genre');
-  var gameLibrary = new GameLibrary('Games Title');
+  $('#addLibrary').submit(function(e){
+    e.preventDefault();
+    var title = $('#newLibraryTitle').val();
+    var gameLib = new GameLibrary(title);
+    gameLib.renderLibrary(gameLib);
+    libraries[gameLib.titleToID()] = gameLib;
+    console.log(libraries);
+  });
 
-  game.render(game);
-  gameLibrary.render(gameLibrary);
-  console.log(game);
-  gameLibrary.addGame(game);
-  console.log(gameLibrary);
-  gameLibrary.removeGame(game);
-  console.log(gameLibrary);
+  // var game = new Game('Travel the World on a Magic Carpet', 'RPG');
+  // var gameLibrary = new GameLibrary('Adventure Games');
+
+  // var game2 = new Game('Candy Crush', 'Puzzle');
+  // var game3 = new Game('Candy Stomp', 'Angry Puzzle');
+  // var gameLibrary2 = new GameLibrary('Puzzle Games');
+
+  // game.render(game);
+  // gameLibrary.addGame(game);
+  // gameLibrary2.addGame(game2);
+  // gameLibrary2.addGame(game3);
+
+  // gameLibrary.render(gameLibrary);
+  // gameLibrary.renderPart2(gameLibrary);
+  // gameLibrary.render(gameLibrary2);
+  // gameLibrary.renderPart2(gameLibrary2);
+
+  // gameLibrary.removeGame(game);
+  // console.log(gameLibrary);
 });
+
+// add games to existing game library or make a newlibrary
+// can delete games and libraries
+// reset form
+// hide form
+// delete games
+// delete libraries
